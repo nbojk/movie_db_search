a77085fe7a36e563574ab518ab08ee85099ddd5f
Neven Bojkovic <nev.enbo@gmail.com>
2020-06-11 02:34:21 +0200

    Final

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,109 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
+    <div class="search">
+      <input type="text" v-on:keyup="updateList" v-model="searchTerm" placeholder="Search...">
+      <select name="type" id="type" v-model="searchType" @click="updateList">
+        <option value="movie">Movie</option>
+        <option value="series">Series</option>
+        <option value="episode">Episode</option>
+      </select>
+    </div>
+    <ul v-if="list">
+      <li v-for="(item, index) in list" v-bind:key="index">
+        <img :src="item.Poster" alt="">
+        <h2> {{ item.Title }} ({{ item.Year }}) </h2>
+        <p> {{ item.Type }} </p>
+      </li>
     </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <p v-if="error"> {{ this.error }} </p>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+    apiKey: 'a92a67d8',
+    searchTerm: '',
+    error: "Search for something.",
+    list: null,
+    searchType: ''
+    }
+  },
+  methods: {
+    updateList: function() {
+      axios.get('http://www.omdbapi.com/?s=' + this.searchTerm  + '&type=' + this.searchType +  '&apikey=a92a67d8')
+      .then((response) => {
+        if(response.data.Response == "True") {
+          this.list = response.data.Search;
+          this.error = null;
+        } else {
+          if(response.data.Error == "Something went wrong." ) {
+            this.error = "Search for something."
+          } else {
+          this.error = response.data.Error;
+          }
+          this.list = null;
+          
+        }
+      })
+      .catch(this.error = "Something went wrong, please try again later.")
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  .hello {
+    width: 800px;
+    margin: 0 auto;
+    overflow: hidden;
+    .search {
+      width: 100%;
+      height: 80px;
+      background-color: #1cbc9b;
+      border-radius: 10px;
+      #type {
+        float: right;
+        border-radius: 10px;
+      }
+      input {
+        vertical-align: center;
+        background-color: #1cbc9b;
+        border: none;
+        color: #2d3e50;
+        width: 600px;
+        height: 75px;
+        font-size: 25px;
+        border-radius: 10px;
+        float: left;
+        font-weight: 700;
+        &:focus {
+          outline: none;
+        }
+      }
+    }
+    ul {
+    list-style-type: none;
+    width: 100%;
+    li {
+      background-color: #2d3e50;
+      margin-right: 75px;
+      border-radius: 10px;
+      p {
+        color: #fff;
+      }
+      h2 {
+        color: #fff;
+      }
+    }
+    } 
+    ::placeholder {
+      color: #2d3e50;
+    }
+  }
 </style>
